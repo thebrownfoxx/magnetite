22b1a1c53eb0111435ae36381953790be06fca32
Jack Norris <hamuelxd@gmail.com>
2025-11-30 13:34:34 +0800

    EnchantmentLevel: Implement some arithmetic operators

--- domain/src/enchantment/level.rs
+++ domain/src/enchantment/level.rs
@@ -1,4 +1,5 @@
 use std::cmp::max;
+use std::ops::{Add, AddAssign, Mul};
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
 pub struct EnchantmentLevel(u8);
@@ -10,7 +11,7 @@ impl EnchantmentLevel {
 
     pub fn combine(self, other: EnchantmentLevel) -> EnchantmentLevel {
         if self == other {
-            return Self::new(self.0 + 1);
+            return self + 1;
         };
 
         max(self, other)
@@ -26,5 +27,27 @@ impl From<u8> for EnchantmentLevel {
 impl From<EnchantmentLevel> for u8 {
     fn from(value: EnchantmentLevel) -> Self {
         value.0
+    }
+}
+
+impl<T: Into<EnchantmentLevel>> Add<T> for EnchantmentLevel {
+    type Output = EnchantmentLevel;
+
+    fn add(self, rhs: T) -> Self::Output {
+        Self::new(self.0 + rhs.into().0)
+    }
+}
+
+impl<T: Into<EnchantmentLevel>> AddAssign<T> for EnchantmentLevel {
+    fn add_assign(&mut self, rhs: T) {
+        self.0 += rhs.into().0;
+    }
+}
+
+impl<T: Into<EnchantmentLevel>> Mul<T> for EnchantmentLevel {
+    type Output = EnchantmentLevel;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::new(self.0 * rhs.into().0)
     }
 }
